b572bc3f132d01e47cbf3eb55ef06ffbd992b3f4
Maciej Wilk <maciej@cdot.app>
2023-03-17 11:21:14 +0100

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder().setTitle('Mobile Reality').build();
+
+  SwaggerModule.setup(
+    'explorer',
+    app,
+    SwaggerModule.createDocument(app, options),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
